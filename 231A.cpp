c7523ee65d09a6bc1d71fc8fb9907443743b92dc
SumitGarg <garg10sumit@gmail.com>
2025-01-30 18:42:20 +0530

    commit

---
+++ 231A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    int count = 0;
+    while (t--)
+    {
+
+        int a, b, c;
+        cin >> a >> b >> c;
+        if ((a == 1 && b == 1) || (b == 1 && c == 1) || (a == 1 && c == 1))
+        {
+            count += 1;
+        }
+    }
+    cout << count << "\n";
+}
